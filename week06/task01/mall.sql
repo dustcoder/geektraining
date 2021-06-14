42650694611ff536c9bc340330034c8b56271966
dusthomeagain@gmail.com <dusthomeagain@gmail.com>
2021-06-14 18:00:42 +0800

    add week06

---
+++ week06/task01/mall.sql
@@ -0,0 +1,63 @@
+CREATE TABLE `t_mall_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户表id',
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(50) NOT NULL COMMENT '用户密码，MD5加密',
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `question` varchar(100) DEFAULT NULL COMMENT '找回密码问题',
+  `answer` varchar(100) DEFAULT NULL COMMENT '找回密码答案',
+  `role` int(4) NOT NULL COMMENT '角色0-管理员,1-普通用户',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '最后一次更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name_unique` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `t_mall_product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '商品id',
+  `category_id` int(11) NOT NULL COMMENT '分类id,对应mmall_category表的主键',
+  `name` varchar(100) NOT NULL COMMENT '商品名称',
+  `subtitle` varchar(200) DEFAULT NULL COMMENT '商品副标题',
+  `main_image` varchar(500) DEFAULT NULL COMMENT '产品主图,url相对地址',
+  `sub_images` text COMMENT '图片地址,json格式,扩展用',
+  `detail` text COMMENT '商品详情',
+  `price` decimal(20,2) NOT NULL COMMENT '价格,单位-元保留两位小数',
+  `stock` int(11) NOT NULL COMMENT '库存数量',
+  `status` int(6) DEFAULT '1' COMMENT '商品状态.1-在售 2-下架 3-删除',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_mall_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '类别Id',
+  `parent_id` int(11) DEFAULT NULL COMMENT '父类别id当id=0时说明是根节点,一级类别',
+  `name` varchar(50) DEFAULT NULL COMMENT '类别名称',
+  `status` tinyint(1) DEFAULT '1' COMMENT '类别状态1-正常,2-已废弃',
+  `sort_order` int(4) DEFAULT NULL COMMENT '排序编号,同类展示顺序,数值相等则自然排序',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100032 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `mmall_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '订单id',
+  `order_no` bigint(20) DEFAULT NULL COMMENT '订单号',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户id',
+  `shipping_id` int(11) DEFAULT NULL,
+  `payment` decimal(20,2) DEFAULT NULL COMMENT '实际付款金额,单位是元,保留两位小数',
+  `payment_type` int(4) DEFAULT NULL COMMENT '支付类型,1-在线支付',
+  `postage` int(10) DEFAULT NULL COMMENT '运费,单位是元',
+  `status` int(10) DEFAULT NULL COMMENT '订单状态:0-已取消，10-未付款，20-已付款，40-已发货，50-交易成功，60-交易关闭',
+  `payment_time` datetime DEFAULT NULL COMMENT '支付时间',
+  `send_time` datetime DEFAULT NULL COMMENT '发货时间',
+  `end_time` datetime DEFAULT NULL COMMENT '交易完成时间',
+  `close_time` datetime DEFAULT NULL COMMENT '交易关闭时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `order_no_index` (`order_no`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8;
+
